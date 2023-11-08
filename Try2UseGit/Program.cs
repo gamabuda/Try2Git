17874641d1b0bbfdbf627595e56313e0800ee641
Koi Den <prostobydet@gmail.com>
2023-11-08 12:49:54 +0300

    fix

--- Try2UseGit/Program.cs
+++ Try2UseGit/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет, мир!");
+﻿string prompt = Console.ReadLine();
+Coffee coffee_client = null;
+if (prompt != null)
+    coffee_client = CoffeeShop.OrderCoffee(prompt);
+
+Americano coffee = coffee_client as Americano;
+Console.WriteLine();
+
+public class Coffee
+{
+    public void grindCoffee()
+    {
+        // перемалываем кофе
+    }
+    public void makeCoffee()
+    {
+        // делаем кофе
+    }
+    public void pourIntoCup()
+    {
+        // наливаем в чашку
+    }
+}
+public class Americano : Coffee { }
+public class Cappuccino : Coffee { }
+public class CaffeLatte : Coffee { }
+public class Espresso : Coffee { }
+
+public static class CoffeeShop
+{
+    public static Coffee OrderCoffee(string type)
+    {
+        Coffee coffee = null;
+
+        switch (type)
+        {
+            case "AMERICANO":
+                coffee = new Americano();
+                break;
+            case "ESPRESSO":
+                coffee = new Espresso();
+                break;
+            case "CAPPUCCINO":
+                coffee = new Cappuccino();
+                break;
+            case "CAFFE_LATTE":
+                coffee = new CaffeLatte();
+                break;
+        }
+
+
+        Console.WriteLine($"Вот ваш кофе {coffee.GetType()}! Спасибо, приходите еще!");
+        return coffee;
+    }
+}
